fab7734b5b26855e6c8cedb4ffb3a58568e7c967
Yevhenii Hyzyla <hyzyla@gmail.com>
2023-09-10 18:01:22 +0200

    Add more exports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export { BaseStream } from "./src/core/base_stream.js";
 export {
   CIRCULAR_REF,
   clearPrimitiveCaches,
@@ -13,6 +14,11 @@ export {
   RefSet,
   RefSetCache,
 } from "./src/core/primitives.js";
+export {
+  DecodeStream,
+  StreamsSequenceStream,
+} from "./src/core/decode_stream.js";
+export { FlateStream } from "./src/core/flate_stream.js";
 export { LocalPdfManager, NetworkPdfManager } from "./src/core/pdf_manager.js";
 export { NullStream, Stream, StringStream } from "./src/core/stream.js";
 export { Page, PDFDocument } from "./src/core/document.js";
