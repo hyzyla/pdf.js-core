7155bf9fefd28a3fb8bcc0ce1ce57408db29713b
Yevhenii Hyzyla <y.hyzyla@vchasno.ua>
2023-10-08 16:16:32 +0200

    Add export of all stram classes 🛀

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+export { Ascii85Stream } from "./src/core/ascii_85_stream.js";
+export { AsciiHexStream } from "./src/core/ascii_hex_stream.js";
 export { BaseStream } from "./src/core/base_stream.js";
+export { CCITTFaxStream } from "./src/core/ccitt_stream.js";
+export {
+  ChunkedStream,
+  ChunkedStreamManager,
+} from "./src/core/chunked_stream.js";
 export {
   CIRCULAR_REF,
   clearPrimitiveCaches,
@@ -18,7 +25,14 @@ export {
   DecodeStream,
   StreamsSequenceStream,
 } from "./src/core/decode_stream.js";
+export { DecryptStream } from "./src/core/decrypt_stream.js";
 export { FlateStream } from "./src/core/flate_stream.js";
+export { Jbig2Stream } from "./src/core/jbig2_stream.js";
+export { JpegStream } from "./src/core/jpeg_stream.js";
+export { JpxStream } from "./src/core/jpx_stream.js";
 export { LocalPdfManager, NetworkPdfManager } from "./src/core/pdf_manager.js";
+export { LZWStream } from "./src/core/lzw_stream.js";
 export { NullStream, Stream, StringStream } from "./src/core/stream.js";
 export { Page, PDFDocument } from "./src/core/document.js";
+export { PredictorStream } from "./src/core/predictor_stream.js";
+export { RunLengthStream } from "./src/core/run_length_stream.js";
